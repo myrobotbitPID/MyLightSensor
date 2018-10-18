87f58e0f04b9997084612e80e7a0b6a849c566f7
myrobotbitPID <42580526+myrobotbitPID@users.noreply.github.com>
2018-10-18 12:32:11 +0700

    Add files via upload

--- MyLightSensor.ts
+++ MyLightSensor.ts
@@ -1,53 +1,34 @@
 /**
-* Coding for read analog sensor.
-*/
-enum sensorSEL {
-	//% block="P0"
-	S0,
-	//% block="P1"
-	S1,
-	//% block="P2"
-	S2,
-	//% block="P3"
-	S3,
-	//% block="P4"
-	S4,
-	//% block="P10"
-	S10
-    }
-
-/**
- * Custom blocks
+ * support for motor
  */
-//% weight=50 color=##006600 icon="\uf11e"
+//% weight=10 icon="\uf21c" color=#FF7210 block="Magibit Sensor"
 namespace MyLightSensor {
 
-     /**
-     * Read Analog Sensor P0-P4,P10 
-     * @param sensorCH sensor's active pin
+    export enum LightSensorPins {
+        P0,
+        P1,
+        P2
+    }
+
+
+    /**
+     * read light sensor's value
+     * @param pin sensor's active pin
      * @return number returns analog value from 0 to 1023
      */
-    //% blockId="MySensor_readSensor" block="analog sensor %sensorSEL"
-    //% weight=100
-    export function readSensor(sensorCH:sensorSEL): number{
-
-	if(sensorCH == sensorSEL.S0){
-	    return pins.analogReadPin(AnalogPin.P0)
-        }
-	if(sensorCH == sensorSEL.S1){
-	    return pins.analogReadPin(AnalogPin.P1)
-        }
-	if(sensorCH == sensorSEL.S2){
-	    return pins.analogReadPin(AnalogPin.P2)
-        }
-	if(sensorCH == sensorSEL.S3){
-	    return pins.analogReadPin(AnalogPin.P3)
-        }
-	if(sensorCH == sensorSEL.S4){
-	    return pins.analogReadPin(AnalogPin.P4)
-        }
-	if(sensorCH == sensorSEL.S10){
-	    return pins.analogReadPin(AnalogPin.P10)
+    //% blockId=magibit_sensor_light_read
+    //% block="Light level read analog at |%pin|"
+    //% weight=80
+    export function lightSensorReadValue(pin: LightSensorPins): number {
+        switch (pin) {
+            case LightSensorPins.P0:
+                return pins.analogReadPin(AnalogPin.P0);
+            case LightSensorPins.P1:
+                return pins.analogReadPin(AnalogPin.P1);
+            case LightSensorPins.P2:
+                return pins.analogReadPin(AnalogPin.P2);
+            default:
+                return 0;
         }
     }
- }
+}
